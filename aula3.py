affbddab7b43f243b6e420f77226dd8a6b94064c
Eduardo Bizerra <Eduardo_Bizerra@hotmail.com>
2023-05-08 15:40:56 -0300

    up
    Primeiras 5 aulas.

---
+++ aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são testos que estão dentro de aspas
+"""
+print (1234)
+
+# Aspas simples
+print('Eduardo Bizerra')
+print(1,'Eduardo "Bizerra"')
+
+#Aspas Duplas
+print("Eduardo Bizerra")
+print(2, "Eduardo ' Bizerra' ")
+
+#Escape
+print("Eduardo \"Bizerra\" ")
+
+# r
+print(r"Eduardo \"Bizerra\"")
---
+++ aula4.py
@@ -0,0 +1,20 @@
+#Tipos Int e float
+#int -> numéro inteiro
+# O tipo int representa qualquer numero
+# O positivo ou negativo. Int sem sinal é considerado
+# positivo.
+print (11) #int
+print(-11) #int
+print(0) #int
+# float -> Numero com ponto flutuante
+# O tipo de float representa qualquer numero
+# positivo ou negativo como ponto flutuante.
+# float sem sinal é considerado positivo.
+print(1.1, 10.11)
+print(0.0, -1.5)
+#A função type mostra o tipo que o Python 
+# inferiu ao valor.
+print(type('Eduardo'))
+print(type(1))
+print(type(1.1))
+print(type(1.1), type(-1.1), type (0.0))
