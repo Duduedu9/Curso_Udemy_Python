6d538352d3b80a788eb3754ae6a9a7bceadb5783
Eduardo Bizerra <Eduardo_Bizerra@hotmail.com>
2023-05-09 19:18:30 -0300

    up

---
+++ aula8.py
@@ -0,0 +1,14 @@
+nome = 'Eduardo' 
+sobrenome = 'Bizerra'
+idade = 26
+ano_nascimento = 1996
+maior_de_idade = idade >= 18
+altura_metros = 1.90
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:' , idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros', altura_metros)
+
+
