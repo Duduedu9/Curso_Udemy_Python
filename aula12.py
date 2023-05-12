61ee2dd17530d249758a3341882f756ae8275b1a
Eduardo Bizerra <Eduardo_Bizerra@hotmail.com>
2023-05-12 16:40:17 -0300

    up

---
+++ aula12.py
@@ -0,0 +1,8 @@
+nome = 'Eduardo Bizerra'
+altura = 1.90
+peso = 87
+imc = peso / altura ** 2
+print ( 'O IMC de', nome, ' é ', imc)
+# Eduardo Bizerra tem 1.90 de altura,
+# pesa 87 kg e seu IMC é
+# 24.099722991689752
