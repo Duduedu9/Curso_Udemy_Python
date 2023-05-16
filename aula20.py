f3e2c45c8b36908b2b97402da5ed64d2836e6e3d
Eduardo Bizerra <Eduardo_Bizerra@hotmail.com>
2023-05-16 10:23:16 -0300

    up

---
+++ aula20.py
@@ -0,0 +1,6 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input ('Digite outro valor: ')
+if primeiro_valor > segundo_valor :
+    print('O primeiro valor é maior que o segundo valor')
+elif segundo_valor > primeiro_valor:
+    print ( 'O segundo valor é maior que o primeiro valor')
