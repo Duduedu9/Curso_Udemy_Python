caf59a87b5baeb18004940409a477ea610f7650b
Eduardo Bizerra <112568948+Duduedu9@users.noreply.github.com>
2023-06-13 13:00:54 -0300

    up

---
+++ aula54.py
@@ -0,0 +1,7 @@
+"""
+Faça uma lista de comprar com listas 
+O usario deve ter a posssibilidade de 
+inserir, apahar e listar valores da sua lista
+Não permita que o programa quebre com 
+erros de índices inexistentes na lista
+"""
